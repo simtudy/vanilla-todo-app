4781a80c3314f59b4590d819d3950ccde840a301
jjanmo <jjanmo07@hanmail.net>
2024-06-06 19:06:50 +0900

    feat: 프로젝트 구성 및 웹팩 설정

--- jjanmo/client/app.js
+++
@@ -1,5 +0,0 @@
-console.log('This is TodoApp');
-
-console.log('test1');
-console.log('test2');
-console.log('test3');
---
+++ jjanmo/client/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isProduction = process.env.NODE_ENV == 'production'
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : 'style-loader'
+
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+
+  devServer: {
+    open: true,
+    hot: true,
+    host: 'localhost',
+    watchFiles: ['src/**/*'],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+}
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production'
+  } else {
+    config.mode = 'development'
+  }
+  return config
+}
