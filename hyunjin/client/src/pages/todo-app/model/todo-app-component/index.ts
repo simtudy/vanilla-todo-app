47894ce86df1c901b98f1a138517230160a00eae
김현진 <guswls1846@naver.com>
2024-06-07 17:13:50 +0900

    feat: todo-bottombar 퍼블리싱

--- hyunjin/client/src/pages/todo-app/model/todo-app-component/index.ts
+++ hyunjin/client/src/pages/todo-app/model/todo-app-component/index.ts
@@ -3,6 +3,7 @@ import { todoHeaderModel } from "@widgets/todo-header";
 import { todoFooterModel } from "@widgets/todo-footer";
 import { todoInputModel } from "@widgets/todo-input";
 import { todoListModel } from "@widgets/todo-list";
+import { todoBottomBarModel } from "@widgets/todo-bottombar";
 export class TodoAppComponent extends HTMLElement {
   constructor() {
     super();
@@ -11,6 +12,7 @@ export class TodoAppComponent extends HTMLElement {
     new todoFooterModel.Component();
     new todoInputModel.Component();
     new todoListModel.Component();
+    new todoBottomBarModel.Component();
   }
 }
 customElements.define("todo-app", TodoAppComponent);
