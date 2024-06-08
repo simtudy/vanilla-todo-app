eb77f691008795cc4dbe7e05a247f1217d7fe53e
jjanmo <jjanmo07@hanmail.net>
2024-06-08 13:53:42 +0900

    feat: flux패턴의 store/reduce/dispatch 간의 로직 구현

--- jjanmo/client/src/store.ts
+++ jjanmo/client/src/store.ts
@@ -1,7 +1,62 @@
-import { State } from '@/types'
+import { State, Action, Todo } from '@/types'
 
 const state: State = {
   todos: [],
+  isAllCompleted: false,
 }
 
-export default state
+const dispatch = <T>({ type, payload }: Action<T>) => {
+  return reducer({ type, payload })
+}
+
+const reducer = ({ type, payload }: Action<unknown>): State => {
+  switch (type) {
+    case 'ADD_TODO': {
+      return {
+        ...state,
+        todos: [...state.todos, payload as Todo],
+      }
+    }
+    case 'EDIT_TODO': {
+      const { id, text, updatedAt } = payload as Pick<Todo, 'id' | 'text' | 'updatedAt'>
+      return {
+        ...state,
+        todos: state.todos.map((todo) => (todo.id === id ? { ...todo, text, updatedAt } : todo)),
+      }
+    }
+    case 'DELETE_TODO': {
+      const { id } = payload as Pick<Todo, 'id'>
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== id),
+      }
+    }
+    case 'CHANGE_TOGGLE_ALL':
+      return {
+        ...state,
+        isAllCompleted: !state.isAllCompleted,
+      }
+    case 'TOGGLE_TODO':
+      const { id } = payload as Pick<Todo, 'id'>
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === id ? { ...todo, status: todo.status === 'active' ? 'completed' : 'active' } : todo
+        ),
+      }
+    case 'TOGGLE_ALL':
+      return {
+        ...state,
+        todos: state.todos.map((todo) => ({ ...todo, status: state.isAllCompleted ? 'completed' : 'active' })),
+      }
+    case 'CLEAR_COMPLETED':
+      return {
+        todos: [],
+        isAllCompleted: false,
+      }
+    default:
+      return state
+  }
+}
+
+export { state, dispatch }
--- jjanmo/client/src/types.ts
+++ jjanmo/client/src/types.ts
@@ -1,6 +1,6 @@
 export type Status = 'active' | 'completed'
 
-export interface ToDo {
+export interface Todo {
   id: string
   text: string
   status: Status
@@ -9,5 +9,20 @@ export interface ToDo {
 }
 
 export interface State {
-  todos: ToDo[]
+  todos: Todo[]
+  isAllCompleted: boolean
+}
+
+export type ActionType =
+  | 'ADD_TODO'
+  | 'EDIT_TODO'
+  | 'DELETE_TODO'
+  | 'CHANGE_TOGGLE_ALL'
+  | 'TOGGLE_TODO'
+  | 'TOGGLE_ALL'
+  | 'CLEAR_COMPLETED'
+
+export interface Action<T> {
+  type: ActionType
+  payload: T
 }
